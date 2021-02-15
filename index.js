d434c8fb95b795cab282e3aa3e6d6e9de81e1fab
psj <park12sj@gmail.com>
2021-02-15 21:21:45 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+const {User} = require('./models/User')
+const config = require('./config/key')
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://psj:poi@134256@psj.e28qs.mongodb.net/psj?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connecte .. '))
   .catch(err => console.log('err'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => res.send('Hello World!!!!!!!'))
+
+app.post('/register', (req, res) =>{
+  // 회원 가입 시 필요한 정보를 db에 넣는다.
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({success: true})
+  })
 })
 
 app.listen(port, () => {
