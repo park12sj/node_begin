f58a22dc0ba2bd5925a9a1d7c4530ef05e097884
psj <park12sj@gmail.com>
2021-02-16 21:57:48 +0900

    login and logout

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const port = 3000
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+const cokieParser = require('cookie-parser')
+app.use(cokieParser())
+
+const {auth} = require('./middleware/auth')
+
 const {User} = require('./models/User')
 const config = require('./config/key')
 
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
@@ -16,12 +23,56 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!!!!!!!'))
 
-app.post('/register', (req, res) =>{
+app.post('/api/user/register', (req, res) =>{
   // 회원 가입 시 필요한 정보를 db에 넣는다.
   const user = new User(req.body)
+
   user.save((err, userInfo) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({success: true})
+  })
+})
+
+app.post('/api/user/login', (req, res) => {
+  //요청 데이터 찾기(mongo에서), 비밀번호 일치 여부, 토큰 생성
+  User.findOne({email: req.body.email}, (err,user)=>{
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        messate : "no user"
+      })
+    } else{
+      user.comparePassword(req.body.password, (err, isMatch)=>{
+        if(!isMatch){
+          return res.json({loginSuccess: false, message: "password wrong"})
+        } else{
+          user.generateToken((err, user) =>{
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token).status(200).json({loginSuccess:true, userId: user._id})
+          })
+        }
+      })
+    }
+  })
+})
+
+app.get('/api/user/auth', auth, (req,res) =>{
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id:req.user._id}, {token: ""}, (err, user)=>{
+    if(err) return res.josn({success:false, err})
+    return res.status(200).send({sucess:true})
   })
 })
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 5
+        minlength: 5
     },
     lastname: {
         type: String,
@@ -30,6 +33,59 @@ const userSchema = mongoose.Schema({
         tupe: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    //비밀번호 암호화
+    user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                // Store hash in your password DB.
+                user.password = hash
+                next()
+            });
+        });
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(planinPassword, cb){
+    var user = this;
+    bcrypt.compare(planinPassword, user.password, function(err, isMatch){
+        if(err) return cb(err)
+        else { return cb(null, isMatch)}
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken') 
+
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+
+userSchema.statics.findByToken = function(token,cb){
+    var user = this
+
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        user.findOne({
+            "_id":decoded,
+            "token":token
+        },function(err,user){
+            if(err) return cb(err)
+            cb(null,user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
